6205b695fb85354655c2949f9968a5da123b8142
Zantzibar <zantzibar@gmx.de>
2018-08-31 08:36:30 +0200

    groundhandling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,31 @@
 
 
 $(document).ready(function() {
+  $(".arrow.up").addClass('hidden');
+
+  $('.arrow.down').click(function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $(window).height()+60 }, 800);
+      //$(".arrow.up").toggle('show');
+  });
+
+  $('.arrow.up').click(function (event) {
+    //$(".arrow.up").toggle('show');
+  });
+
 
   $(window).on('scroll', function() {
 
-    if (document.body.scrollTop > document.documentElement.clientHeight || document.documentElement.scrollTop > document.documentElement.clientHeight) {
+    if (document.body.scrollTop > document.documentElement.clientHeight/3 || document.documentElement.scrollTop > document.documentElement.clientHeight/3) {
       document.getElementById("navbar").style.top = "0";
+      $(".arrow.up").removeClass('hidden');
+      $(".arrow.up").removeClass('hidden');
+      $(".container").addClass('hidden');
     } else {
       document.getElementById("navbar").style.top = "-60px";
+      $(".arrow.up").addClass('hidden');
+      $(".container").removeClass('hidden');
     }
   });
 
